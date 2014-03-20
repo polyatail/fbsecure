6155c6e9079bbf3753cce8d9c9d7dc6ef1786908
Andrew Sczesnak <andrewscz@gmail.com>
2014-03-19 18:01:58 -0700

    Hackhackhack

--- content_script.js
+++ content_script.js
@@ -24,57 +24,57 @@ setInterval(function () {
   {
     $(document).data("posts_examined", 0);
     $(document).data("stored_url", doc_url);
-  }
-
-  // find text content within posts
-  var fbsPostText = $('div.mbs._5pbx.userContent');
-
-  if (fbsPostText.length == 0)
-  {
-    var timeline = $('div._4_7u')[0];
-    var fbsPostText = $('div.userContentWrapper', timeline);
-  }
-
-  // has new content been loaded?
-  console.log($(document).data("posts_examined") + ", " + fbsPostText.length);
-
-  if ($(document).data("posts_examined") < fbsPostText.length)
-  {
-    $('<link rel="stylesheet" type="text/css" href="'+chrome.extension.getURL("fbsecure.css")+'" >').appendTo("head");
-
-    // search for PGP encrypted text
-    for (i = $(document).data("posts_examined"); i < fbsPostText.length; i++)
+  } else {
+    // find text content within posts
+    var fbsPostText = $('div.mbs._5pbx.userContent');
+  
+    if (fbsPostText.length == 0)
     {
-      $(document).data("posts_examined", fbsPostText.length);
+      var timeline = $('div._4_7u')[0];
+      var fbsPostText = $('div.userContentWrapper', timeline);
+    }
   
-      if (fbsPostText[i].innerText.substring(0, 27) == "-----BEGIN PGP MESSAGE-----")
+    // has new content been loaded?
+    console.log($(document).data("posts_examined") + ", " + fbsPostText.length);
+  
+    if ($(document).data("posts_examined") < fbsPostText.length)
+    {
+      $('<link rel="stylesheet" type="text/css" href="'+chrome.extension.getURL("fbsecure.css")+'" >').appendTo("head");
+  
+      // search for PGP encrypted text
+      for (i = $(document).data("posts_examined"); i < fbsPostText.length; i++)
       {
-        exposed_root = $('div.text_exposed_root', fbsPostText[i]);
-        exposed_show = $('span.text_exposed_show', fbsPostText[i]);
-  
-        exposed_root = exposed_root[0].innerText;
-        exposed_root = exposed_root.replace("...\nSee More\n", "").replace("...See More", "");
-  
-        exposed_show = exposed_show[0].innerText;
-        exposed_show = exposed_show.replace(/\n /g, "\n");
-  
-        pgpmsg = exposed_root + exposed_show;
-  
-        console.log(i + ":" + pgpmsg);
-        decodedMessage = openpgp.read_message(pgpmsg);
-  
-        var totalMessage = "";
-   
-        if (decodedMessage)
-        { 
-          for (j = 0; j < decodedMessage.length; j++)
-          {
-            totalMessage += decodedMessage[j].text;
+        $(document).data("posts_examined", fbsPostText.length);
+    
+        if (fbsPostText[i].innerText.substring(0, 27) == "-----BEGIN PGP MESSAGE-----")
+        {
+          exposed_root = $('div.text_exposed_root', fbsPostText[i]);
+          exposed_show = $('span.text_exposed_show', fbsPostText[i]);
+    
+          exposed_root = exposed_root[0].innerText;
+          exposed_root = exposed_root.replace("...\nSee More\n", "").replace("...See More", "");
+    
+          exposed_show = exposed_show[0].innerText;
+          exposed_show = exposed_show.replace(/\n /g, "\n");
+    
+          pgpmsg = exposed_root + exposed_show;
+    
+          console.log(i + ":" + pgpmsg);
+          decodedMessage = openpgp.read_message(pgpmsg);
+    
+          var totalMessage = "";
+     
+          if (decodedMessage)
+          { 
+            for (j = 0; j < decodedMessage.length; j++)
+            {
+              totalMessage += decodedMessage[j].text;
+            }
+    
+            $(fbsPostText[i]).html("<div class='for_whose_eyes'><b>Visible Only To You</b></div>" + totalMessage);
           }
-  
-          $(fbsPostText[i]).html("<div class='for_whose_eyes'><b>Visible Only To You</b></div>" + totalMessage);
         }
       }
     }
   }
-}, 2000);
+}, 1000);
